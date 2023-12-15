37c4c5f2d4f9e776c00d3b4794bd4d9a25fd5635
Chris Lorenzo <christopher_lorenzo@comcast.com>
2023-12-15 00:37:03 -0500

    set width + height if text is contained

--- src/core/node/index.ts
+++ src/core/node/index.ts
@@ -505,6 +505,23 @@ export class ElementNode extends Object {
         ...props,
         text: node.getText(),
       };
+
+      if (props.contain) {
+        if (!props.width) {
+          props.width =
+            (parent.width || 0) - props.x - (props.marginRight || 0);
+          node._width = props.width;
+          node._autosized = true;
+        }
+
+        if (!props.height && props.contain === 'both') {
+          props.height =
+            (parent.height || 0) - props.y - (props.marginBottom || 0);
+          node._height = props.height;
+          node._autosized = true;
+        }
+      }
+
       log('Rendering: ', this, props);
       node.lng = renderer.createTextNode(props);
 
